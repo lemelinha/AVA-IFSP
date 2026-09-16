ef5aa8e48de95e4cb5effb4e04010d8666a65ce1
Lucas Leme <67035515+lemelinha@users.noreply.github.com>
2026-09-16 18:33:19 +0000

    codiguracao do vite e pagina de login

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,8 @@ import laravel from 'laravel-vite-plugin';
 import { bunny } from 'laravel-vite-plugin/fonts';
 import { defineConfig, lazyPlugins } from 'vite-plus';
 
+const isCodespace = !!process.env.CODESPACE_NAME;
+
 export default defineConfig({
     plugins: lazyPlugins(() => [
         laravel({
@@ -17,7 +19,14 @@ export default defineConfig({
         tailwindcss(),
     ]),
     server: {
+        host: '0.0.0.0',
+        port: 5173,
         cors: true,
+        hmr: {
+            host: isCodespace ? `${process.env.CODESPACE_NAME}-5173.app.github.dev` : 'localhost',
+            protocol: isCodespace ? 'wss' : 'ws',
+            clientPort: isCodespace ? 443 : 5173
+        },
         watch: {
             ignored: [
                 '**/.agents/**',
